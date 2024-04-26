46a2b7b10a5012fcb7a0d37e291ab248b6cd3a75
sony <sony.sth8@gmail.com>
2024-04-26 23:32:09 +0200

    Code for ABOX added

--- abox.py
+++ abox.py
@@ -2,29 +2,11 @@ import pandas as pd
 from rdflib import Graph, URIRef, Literal, Namespace, RDF, XSD
 
 # Define namespaces
-keyword_pub = Namespace("http://www.example.edu/publication/keyword/")
-organization_pub = Namespace("http://www.example.edu/publication/organization/")
+pub = Namespace("http://www.example.edu/publication/")
 xsd = Namespace("http://www.w3.org/2001/XMLSchema#")
 
 # Create an RDF graph
 g = Graph()
-
-def keywords():
-    keywords_df = pd.read_csv('./data/keywords.csv')
-
-    for index, row in keywords_df.iterrows():
-        subject = URIRef(keyword_pub + row['keywords'].replace(" ","_"))
-        predicate = RDF.type
-        obj = URIRef('Keyword')
-        
-        keyword_literal = Literal(row['keywords'], datatype = XSD.string)
-        
-        # Add triples to the RDF graph
-        g.add((subject, predicate, obj))
-        g.add((subject, keyword_pub.keyword, keyword_literal))
-
-    # Serialize the RDF graph to Turtle format and print
-    print(g.serialize(format="turtle"))
 
 
 
@@ -32,22 +14,253 @@ def organizations():
     organization_df = pd.read_csv('./data/organizations.csv')
 
     for index, row in organization_df.iterrows():
-        subject = URIRef(organization_pub + row['name'].replace(" ",""))
+        subject = URIRef(pub + 'organizations/'+str(row['id']))
         predicate = RDF.type
-        obj = URIRef('Organization')
+        obj = pub.Organization
 
         org_name_literal = Literal(row['name'], datatype = XSD.string)
         org_type_literal = Literal(row['type'], datatype = XSD.string)
         
         # Add triples to the RDF graph
         g.add((subject, predicate, obj))
-        g.add((subject, keyword_pub.org_name, org_name_literal))
-        g.add((subject, keyword_pub.org_type, org_type_literal))
+        g.add((subject, pub.org_name, org_name_literal))
+        g.add((subject, pub.org_type, org_type_literal))
+
+    # Serialize the RDF graph to Turtle format and print
+    print(g.serialize(format="turtle"))
+
+
+
+def authors():
+    author_df = pd.read_csv('./data/authors.csv')
+
+    for index, row in author_df.iterrows():
+        subject = URIRef(pub + 'authors/'+str(row['authorId']))
+        predicate = RDF.type
+        obj = pub.Author
+
+        author_name_literal = Literal(row['authorName'], datatype = XSD.string) 
+        author_aff_org_literal = URIRef(pub + 'organization/'+str(row['affiliatedOrg']))
+        
+        # Add triples to the RDF graph
+        g.add((subject, predicate, obj))
+        g.add((subject, pub.author_name, author_name_literal))
+        g.add((subject, pub.affiliated_org, author_aff_org_literal))
+
+    # Serialize the RDF graph to Turtle format and print
+    print(g.serialize(format="turtle"))
+
+
+
+def keywords():
+    keywords_df = pd.read_csv('./data/keywords.csv')
+
+    for index, row in keywords_df.iterrows():
+        subject = URIRef(pub + 'keywords/'+row['keywords'].replace(" ","_"))
+        predicate = RDF.type
+        obj = pub.Keyword
+        
+        keyword_literal = Literal(row['keywords'], datatype = XSD.string)
+        
+        # Add triples to the RDF graph
+        g.add((subject, predicate, obj))
+        g.add((subject, pub.keyword, keyword_literal))
+
+    # Serialize the RDF graph to Turtle format and print
+    print(g.serialize(format="turtle"))
+
+
+
+def journals():
+    journal_df = pd.read_csv('./data/journals.csv')
+
+    for index, row in journal_df.iterrows():
+        subject = URIRef(pub + 'journals/'+ str(row['id']))
+        predicate = RDF.type
+        obj = pub.Journal
+
+        journal_name_literal = Literal(row['jcwName'], datatype = XSD.string)
+        
+        # Add triples to the RDF graph
+        g.add((subject, predicate, obj))
+        g.add((subject, pub.journal_name, journal_name_literal))
+
+    # Serialize the RDF graph to Turtle format and print
+    print(g.serialize(format="turtle"))
+
+
+
+def workshops():
+    workshop_df = pd.read_csv('./data/workshops.csv')
+
+    for index, row in workshop_df.iterrows():
+        subject = URIRef(pub + 'workshops/' + str(row['id']))
+        predicate = RDF.type
+        obj = pub.Workshop
+
+        workshop_name_literal = Literal(row['jcwName'], datatype = XSD.string)
+        
+        # Add triples to the RDF graph
+        g.add((subject, predicate, obj))
+        g.add((subject, pub.workshop_name, workshop_name_literal))
+
+    # Serialize the RDF graph to Turtle format and print
+    print(g.serialize(format="turtle"))
+
+
+
+def conferences():
+    conference_df = pd.read_csv('./data/workshops.csv')
+
+    for index, row in conference_df.iterrows():
+        subject = URIRef(pub + 'workshops/' + str(row['id']))
+        predicate = RDF.type
+        obj = pub.Conference
+
+        conference_name_literal = Literal(row['jcwName'], datatype = XSD.string)
+        
+        # Add triples to the RDF graph
+        g.add((subject, predicate, obj))
+        g.add((subject, pub.conference_name, conference_name_literal))
+
+    # Serialize the RDF graph to Turtle format and print
+    print(g.serialize(format="turtle"))
+
+
+
+def conference_paper():
+    conference_paper_df = pd.read_csv('./data/conference_paper.csv')
+
+    for index, row in conference_paper_df.iterrows():
+        subject = URIRef(pub + 'conference_paper/'+row['paperId'])
+        predicate = RDF.type
+        obj = pub.Paper
+
+        title_literal = Literal(row['title'], datatype = XSD.string)
+        abstract_literal = Literal(row['abstract'], datatype = XSD.string)
+        year_literal = Literal(row['year'], datatype = XSD.int)
+        citation_count_literal = Literal(row['citationCount'], datatype = XSD.int)
+        publication_date_literal = Literal(row['publicationDate'], datatype = XSD.date)
+        edition_literal = Literal(row['edition'], datatype = XSD.string)
+        
+        # Add triples to the RDF graph
+        g.add((subject, predicate, obj))
+        g.add((subject, pub.title, title_literal))
+        g.add((subject, pub.abstract, abstract_literal))
+        g.add((subject, pub.year, year_literal))
+        g.add((subject, pub.citation_count, citation_count_literal))
+        g.add((subject, pub.publication_date, publication_date_literal))
+        
+        g.add((subject, pub.corresponding_author, URIRef(pub + 'author/'+str(row['correspondingAuthorId']))))
+        g.add((subject, pub.edition, edition_literal))
+
+        g.add((subject, pub.published_in, URIRef(pub + 'conference/' + str(row['jcwName']))))
+
+        for keyword in row["keywords"].split(","):
+            g.add((subject, pub.keyword, URIRef(pub + 'keyword/'+str(keyword.strip().replace(" ","_")))))
+
+        for author in row["authorId"].split(","):
+            g.add((subject, pub.written_by, URIRef(pub + 'author/'+str(author))))
+        
+        for cited_paper_id in row["citedPaperId"].split(","):
+            g.add((subject, pub.cites, URIRef(pub + 'paper/'+str(cited_paper_id))))
+        
+        for reviewer in row["reviewers"].split(","):
+            g.add((subject, pub.reviwed_by, URIRef(pub + 'author/'+str(reviewer))))
+
+    # Serialize the RDF graph to Turtle format and print
+    print(g.serialize(format="turtle"))
+
+
+def workshop_paper():
+    workshop_paper_df = pd.read_csv('./data/workshop_paper.csv')
+
+    for index, row in workshop_paper_df.iterrows():
+        subject = URIRef(pub + 'workshop_paper/'+row['paperId'])
+        predicate = RDF.type
+        obj = pub.Paper
+
+        title_literal = Literal(row['title'], datatype = XSD.string)
+        abstract_literal = Literal(row['abstract'], datatype = XSD.string)
+        year_literal = Literal(row['year'], datatype = XSD.int)
+        citation_count_literal = Literal(row['citationCount'], datatype = XSD.int)
+        publication_date_literal = Literal(row['publicationDate'], datatype = XSD.date)
+        edition_literal = Literal(row['edition'], datatype = XSD.string)
+        
+        # Add triples to the RDF graph
+        g.add((subject, predicate, obj))
+        g.add((subject, pub.title, title_literal))
+        g.add((subject, pub.abstract, abstract_literal))
+        g.add((subject, pub.year, year_literal))
+        g.add((subject, pub.citation_count, citation_count_literal))
+        g.add((subject, pub.publication_date, publication_date_literal))
+        g.add((subject, pub.corresponding_author, URIRef(pub + 'author/'+str(row['correspondingAuthorId']))))
+        g.add((subject, pub.edition, edition_literal))
+
+        g.add((subject, pub.published_in, URIRef(pub + 'workshop/'+ str(row['jcwName']))))
+
+        for keyword in row["keywords"].split(","):
+            g.add((subject, pub.keyword, URIRef(pub + 'keyword/'+str(keyword.strip().replace(" ","_")))))
+
+        for author in row["authorId"].split(","):
+            g.add((subject, pub.written_by, URIRef(pub + 'author/'+str(author))))
+        
+        if pd.notna(row['citedPaperId']):
+            for cited_paper_id in row["citedPaperId"].split(","):
+                g.add((subject, pub.cites, URIRef(pub + 'paper/'+str(cited_paper_id))))
+        
+        for reviewer in row["reviewers"].split(","):
+            g.add((subject, pub.reviwed_by, URIRef(pub + 'author/'+str(reviewer))))
+
+    # Serialize the RDF graph to Turtle format and print
+    print(g.serialize(format="turtle"))
+
+
+
+def journal_paper():
+    journal_paper_df = pd.read_csv('./data/journal_paper.csv')
+
+    for index, row in journal_paper_df.iterrows():
+        subject = URIRef(pub + 'journal_paper/'+row['paperId'])
+        predicate = RDF.type
+        obj = pub.Paper
+
+        title_literal = Literal(row['title'], datatype = XSD.string)
+        abstract_literal = Literal(row['abstract'], datatype = XSD.string)
+        year_literal = Literal(row['year'], datatype = XSD.int)
+        citation_count_literal = Literal(row['citationCount'], datatype = XSD.int)
+        publication_date_literal = Literal(row['publicationDate'], datatype = XSD.date)
+        journal_volume_literal = Literal(row['journalVolume'], datatype = XSD.string)
+        
+        # Add triples to the RDF graph
+        g.add((subject, predicate, obj))
+        g.add((subject, pub.title, title_literal))
+        g.add((subject, pub.abstract, abstract_literal))
+        g.add((subject, pub.year, year_literal))
+        g.add((subject, pub.citation_count, citation_count_literal))
+        g.add((subject, pub.publication_date, publication_date_literal))
+        g.add((subject, pub.corresponding_author, URIRef(pub + 'author/'+str(row['correspondingAuthorId']))))
+        g.add((subject, pub.journal_volume, journal_volume_literal))
+
+        g.add((subject, pub.published_in, URIRef(pub + 'journal/' + str(row['jcwName']))))
+
+        for keyword in row["keywords"].split(","):
+            g.add((subject, pub.keyword, URIRef(pub + 'keyword/'+str(keyword.strip().replace(" ","_")))))
+
+        for author in row["authorId"].split(","):
+            g.add((subject, pub.written_by, URIRef(pub + 'author/'+str(author))))
+        
+        if pd.notna(row['citedPaperId']):
+            for cited_paper_id in row["citedPaperId"].split(","):
+                g.add((subject, pub.cites, URIRef(pub + 'paper/'+str(cited_paper_id))))
+        
+        for reviewer in row["reviewers"].split(","):
+            g.add((subject, pub.reviwed_by, URIRef(pub + 'author/'+str(reviewer))))
 
     # Serialize the RDF graph to Turtle format and print
     print(g.serialize(format="turtle"))
 
 
 if __name__ == "__main__":
-    keywords()
-    # organizations()
+    # keywords()
+    workshop_paper()
